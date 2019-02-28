dacfa8e06a02f952805e16740d87b5242e0d3084
affliction001 <affliction001@mail.ru>
2019-02-28 20:23:04 +0300

    l2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // Macros
 const $ = selector => document.querySelector(selector);
 
-ymaps.ready(init);
+let balloonLayout = '<div><h1>Balloon title</h1><p>Balloon description must be here...</p></div>';
 
 function init() {
 	let myMap = new ymaps.Map('map', {
@@ -12,14 +12,41 @@ function init() {
 		zoom: 15
 	});
 
-	let placemark1 = new ymaps.Placemark([55.796782, 49.099031], {
-		baloonContent: '<p>Центральный манеж</p>',
-		iconContent: 'Манеж'
+	function MyBehavior() {
+		this.options = new ymaps.option.Manager();
+		this.events = new ymaps.event.Manager();
+	}
+
+	MyBehavior.prototype = {
+	    constructor: MyBehavior,
+	    enable: function () {
+	        this._parent.getMap().events.add('click', this._onClick, this);
+	    },
+	    disable: function () {
+	        this._parent.getMap().events.remove('click', this._onClick, this);
+	    },
+	    setParent: function (parent) { this._parent = parent; },
+	    getParent: function () { return this._parent; },
+	    _onClick: function (e) {
+	        var coords = e.get('coords');
+	        // this._parent.getMap().setCenter(coords); //Сдвигает центр карты к месту клика.
+	        console.log(coords);
+	        createPlacemark(myMap, coords, balloonLayout);
+	    }
+	};
+
+	ymaps.behavior.storage.add('mybehavior', MyBehavior);
+	myMap.behaviors.enable('mybehavior');
+}
+
+function createPlacemark(map, coordinates, content) {
+	let placemark = new ymaps.Placemark(coordinates, {
+		balloonContent: content
 	}, {
 		preset: 'islands#yellowStretchyIcon',
-		baloonCloseButton: true,
-		hideIconOnBaloonOpen: false
+		balloonCloseButton: true,
+		hideIconOnBalloonOpen: false
 	});
 
-	myMap.geoObjects.add(placemark1);
+	map.geoObjects.add(placemark);
 }
